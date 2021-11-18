3c3447ca1b2170ac6741e9293910bc761b226775
michiel_van_overmeire <michiel.vanovermeire@experius.nl>
2021-11-18 10:33:55 +0100

    Added code to calculator class

--- calculator.php
+++ calculator.php
@@ -2,15 +2,51 @@
 
 class Calculator {
 
-public function add(){
-    
+public function add($number1, $number2){
+
+    if(is_numeric($number1) && is_numeric($number2)) {
+        return $number1 + $number2;
+    } else{
+        echo 'Both values should be numeric';
+    }
+}
+
+public function subtract($number1, $number2){
+
+    if(is_numeric($number1) && is_numeric($number2)) {
+        return $number1 - $number2;
+    } else{
+        echo 'Both values should be numeric';
+    }
+}
+
+public function multiply($number1,$number2){
+
+    if(is_numeric($number1) && is_numeric($number2)) {
+        return $number2 * $number1;
+    } else{
+        echo 'Both values should be numeric';
+    }
 }
 
 
+public function divide($number1, $number2){
 
-
-
-
+    if(is_numeric($number1) && is_numeric($number2)) {
+        return $number1 / $number2;
+    } else{
+        echo 'Both values should be numeric';
+    }
+}
 
 
 }
+
+$calc = new Calculator();
+echo $calc->add(8,2);
+echo $calc->subtract(8,2);
+echo $calc->multiply(8,2);
+echo $calc->divide(8,2);
+
+
+?>
