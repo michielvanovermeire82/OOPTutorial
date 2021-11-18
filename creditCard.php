7a72c233f9a8f80515808e94ed3e1a3270c4125f
michiel_van_overmeire <michiel.vanovermeire@experius.nl>
2021-11-18 13:07:13 +0100

    Added withdrawMoney() function to CreditCard class

--- creditCard.php
+++ creditCard.php
@@ -33,19 +33,35 @@ public function getPin(){
 
 public function shopping($cardNumber, $pinNumber, $spendingMoney){
     if($cardNumber === $this->getCardNumber() && $pinNumber === $this->getPin()){
-        if($spendingMoney -= $this->getTotalBalance($this->totalBalance)){
-            $remBalance = $spendingMoney - $this->getTotalBalance($this->totalBalance);
+        if($spendingMoney <= $this->getTotalBalance($this->totalBalance)){
+            $remBalance = $this->getTotalBalance($this->totalBalance) - $spendingMoney;
             $this->setTotalBalance($remBalance);
-            echo "You have spent" . $spendingMoney . "and your remaning balance is " . $remBalance . ".";
+            echo "You have spent " . $spendingMoney . " and your remaning balance is " . $remBalance . ".";
         } else{
-            echo " You dont have enough balance in your account";
+            echo " You don't have enough balance in your account!";
         }
     } else {
-        echo "Your carnd number or pin is invalid";
+        echo "Your card number or pin is invalid!";
     }
 }
+
+public function withdrawMoney($cardNumber, $pinNumber, $withdrawMoney){
+    if($cardNumber === $this->getCardNumber() && $pinNumber === $this->getPin()){
+        $withdrawMoney = $withdrawMoney + $withdrawMoney * (3/100);
+        if($withdrawMoney <= $this->getTotalBalance($this->totalBalance)){
+            $remBalance = $this->getTotalBalance($this->totalBalance) - $withdrawMoney;
+            $this->setTotalBalance($remBalance);
+            echo "You have withdrawn " . $withdrawMoney . " and your remaning balance is " . $remBalance . ".";
+        } else {
+            echo " You don't have enough balance in your account!";
+         }    
+    } else {
+        echo "Your card number or pin is invalid!";
+    }
+}
+
 }
 
 $creditcard = new CreditCard();
 $creditcard->setPin(4321);
-$creditcard->shopping(1111222233334444, 4321, 550);
+$creditcard->withdrawMoney(1111222233334444, 1234, 100);
